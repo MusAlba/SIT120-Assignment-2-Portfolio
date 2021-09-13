d064d1d954a0a02827e45d9bcb9121381afea4bd
Mustafa Saleh <90378821+MusAlba@users.noreply.github.com>
2021-09-13 20:50:31 +1000

    Create main.js

---
+++ Week 1/main.js
@@ -0,0 +1,12 @@
+
+	/**Adding javascript***/
+  document.getElementById("intro").innerHTML="HTML is a markup language";
+  
+  /**Adding vue**/
+ var app = new Vue ({
+ el:' #app',
+ data: {
+ 		title: ' COURSE:SIT120 INTRODUCTION TO REPSONSIVE WEB APPS'
+ }
+ 
+ })
