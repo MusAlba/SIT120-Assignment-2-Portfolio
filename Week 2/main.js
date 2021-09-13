5bfba8741910d0380d065918273715dd659e6176
Mustafa Saleh <90378821+MusAlba@users.noreply.github.com>
2021-09-14 05:45:16 +1000

    Create main.js

---
+++ Week 2/main.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+	el: "#app",
+  data: {
+  	title: 'Student QuickCare',
+    description: 'Is meant to help students and collegestakeholders collaborative in improving services at the institution by addressing students grievances as fast as possible',
+    objectivetitle: 'Objectives:',
+    objectives: [
+    	{text: 'To reduce turn around time for addressing student grievances.'},
+      {text: 'To make the student experience at the institution friendly and foster stronger student staff relationship'},
+      {text: 'To foster the institutions image and core values by setting a good example in student care'}
+    ]
+    
+  }
+})
